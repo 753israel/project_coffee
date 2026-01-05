4c7f5dc9115ef28aa5b03897eee2dd9c1a0885e0
israel <israelb300790@gmail.com>
2026-01-05 20:34:33 +0200

    update user controlers

--- backend/main.py
+++ backend/main.py
@@ -1,2 +1,34 @@
+from flask_cors import CORS
 from flask import Flask
+from backend.src.routes.user_route import users_bp
+from backend.src.database import init_database, create_tables, drop_tables
 
+# flask - http פריימוורק שנותן שרת 
+# routes כולל תמיכה ב 
+
+# 1) create flask
+app = Flask(__name__)
+
+# postgresql מוסיפים לפלאסק את נתיב החיבור ל
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://coffee_db:12345@10.0.0.10:5432/coffee_user"
+
+# כאשר דפדפן שמחובר לשרת אחר שולח בקשה לשרת הזה
+# headers מורה לפלאסק לשלוח כתשובה 
+# חזרה לדפדפן ששלח את הבקשה
+# אחרת הדפדפן חוסם את התשובה
+CORS(app)
+
+# כדי להורות לפלאסק להעביר בקשות לראוט
+app.register_blueprint(users_bp)
+
+# database  חיבור ל
+init_database(app)
+
+# יצירת טבלאות אם אינן קיימות
+with app.app_context():
+    # drop_tables()
+    create_tables()
+
+# run flask
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)  #https מריץ את פלאסק
